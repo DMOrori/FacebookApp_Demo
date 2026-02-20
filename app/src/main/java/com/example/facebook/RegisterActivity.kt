ae8b03ced87bfda410438de670395182022196cf
derrick-maebar <derrickorori@gmail.com>
2026-02-20 16:13:21 +0300

    initial commit

---
+++ app/src/main/java/com/example/facebook/RegisterActivity.kt
@@ -0,0 +1,37 @@
+package com.example.facebook
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val tvHeading = findViewById<TextView>(R.id.tvHeading)
+        val etName = findViewById<EditText>(R.id.etName)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val tvConfirmPassword = findViewById<TextView>(R.id.tvConfirmPassword)
+
+        btnRegister.setOnClickListener {
+            Toast.makeText(this,"Registration successfull!",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
